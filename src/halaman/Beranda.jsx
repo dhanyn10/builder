0c06d72c3a65e1cca489aadff3410905a998cc3b
dhanyn10 <dhanyn.dhan@gmail.com>
2017-10-27 14:16:12 +0700

    tambah beberapa proyek

--- src/halaman/Beranda.jsx
+++ src/halaman/Beranda.jsx
@@ -52,6 +52,70 @@ class Beranda extends React.Component
                                 </CardBody>
                             </Card>
                         </div>
+                        <div className="col-md-4">
+                            <Card>
+                                <CardHeader className="bg-secondary text-white">
+                                    Alfa
+                                </CardHeader>
+                                <CardBody>
+                                    Online CV maker for everyone
+                                    <hr/>
+                                    <div className="btn-group">
+                                        <Button className="btn btn-sm btn-danger">Developed</Button>
+                                        <Button className="btn btn-sm btn-danger">Closed Source</Button>
+                                        <Button className="btn btn-sm btn-secondary">Private</Button>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-md-4">
+                            <Card>
+                                <CardHeader className="bg-secondary text-white">
+                                    Laravel google login
+                                </CardHeader>
+                                <CardBody>
+                                    Simple implementation third party service login with google
+                                    <hr/>
+                                    <div className="btn-group">
+                                        <Button className="btn btn-sm btn-success">ready</Button>
+                                        <Button className="btn btn-sm btn-success">Open Source</Button>
+                                        <Button className="btn btn-sm btn-success">public</Button>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-md-4">
+                            <Card>
+                                <CardHeader className="bg-secondary text-white">
+                                    Simple chat SocketIO
+                                </CardHeader>
+                                <CardBody>
+                                    simple chat using socket.io, with several effects 
+                                    <hr/>
+                                    <div className="btn-group">
+                                        <Button className="btn btn-sm btn-success">ready</Button>
+                                        <Button className="btn btn-sm btn-success">Open Source</Button>
+                                        <Button className="btn btn-sm btn-success">public</Button>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-md-4">
+                            <Card>
+                                <CardHeader className="bg-secondary text-white">
+                                    MatematikaJS
+                                </CardHeader>
+                                <CardBody>
+                                    Opensource Javascript Library that allows user in client side to make calculation 
+                                    <hr/>
+                                    <div className="btn-group">
+                                        <Button className="btn btn-sm btn-secondary">developed</Button>
+                                        <Button className="btn btn-sm btn-success">Open Source</Button>
+                                        <Button className="btn btn-sm btn-success">public</Button>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </div>
                     </Row>
                 </Container>
             </div>
